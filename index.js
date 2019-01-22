c6bc1d8e84dbd7c1dac7f079a1c7af928edb9b9e
Ndubuisi Samuel <samniwebdev@gmail.com>
2019-01-22 12:44:39 +0100

    Added payload parser

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 //Core Node Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
@@ -26,18 +27,32 @@ const server = http.createServer((req, res) => {
     // Get the headers as an object
     let headers = req.headers;
 
-    // Send the response
-    res.end('Hello World\n');
 
-    // Log the request path
-    console.log(`Request received on path:`, trimmedPath);
-    console.log(`Request received with method:`, method);
-    console.log(`Request received with query string parameters:`, queryStringObject);
-    console.log(`Request received with headers:`, headers);
+    let decoder = new StringDecoder('utf-8');
+    let buffer = '';
 
+    // Parsing payload
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
 
+    req.on('end', () => {
+        buffer += decoder.end();
 
-})
+        // At request end, send response
+
+        // Send the response
+        res.end('Hello World\n');
+
+        // Log the request path
+        console.log(`Request received on path:`, trimmedPath);
+        console.log(`Request received with method:`, method);
+        console.log(`Request received with query string parameters:`, queryStringObject);
+        console.log(`Request received with headers:`, headers);
+        console.log('Request received with payload:', buffer);
+    });
+
+});
 
 // Start the server, and have it listen on port 3000
 server.listen(5000, () => {
